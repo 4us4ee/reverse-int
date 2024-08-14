75d56713d8056f4420858448a9cc574dc8e3638c
Vika Usova <getsbistorm1@mail.ru>
2024-08-15 01:14:08 +0700

    docs:index.js done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = '';
+    if(n<0){
+        n= n-n-n;
+    }
+    let  x = String(n);
+for(let i=x.length-1;i>-1;i--){
+    str+=x[i];
+}
+return Number(str);
 }
